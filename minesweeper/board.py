cb51b6566566ef0ebdeca41ed7bb97252219440c
alexhlanter <alexhlanter@gmail.com>
2026-09-14 20:00:45 -0500

    Added drawing number of adjacent mines to board.py

--- minesweeper/board.py
+++ minesweeper/board.py
@@ -18,7 +18,7 @@ class Board:
         self.x = 25
         self.y = 25
         self.game = Minesweeper(num_mines, rows, cols)  # Initialize the Minesweeper game with 10 mines
-
+        self.font = pygame.font.Font(None, 32)  # font for drawing adjacent-mine numbers
 
     def handle_click(self, mouse_x, mouse_y):
         #convert mouse click position to board coordinates
@@ -63,3 +63,11 @@ class Board:
                     "black",
                     (x, y, self.tile_size, self.tile_size), 2
                 )
+
+                #Draw number adjacent mines (unless=0)
+                if (col, row) in self.game.revealed and (col, row) not in self.game.mines:
+                    count = self.game.adjacent_mines.get((col, row), 0) #Get mine number
+                    if count > 0:
+                        text = self.font.render(str(count), True, "black") #Draw the number of mines to middle of tile
+                        text_rect = text.get_rect(center=(x + self.tile_size // 2, y + self.tile_size // 2))
+                        screen.blit(text, text_rect)
